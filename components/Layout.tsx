9c1482eba2daada3fa27a82cf65b714f75b8d937
Valdemiro Viegas <valdemiroviegas@yahoo.com>
2022-10-01 14:39:02 +0100

    4

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -56,7 +56,7 @@ function Layout({children, showNavbar}: IProps) {
         }
   
       
-        <div className={`z-[100] overflow-y-scroll overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 ${showNavbar ? "h-[93vh]" : "h-[100vh]"} md:${showNavbar ? "left-[13vw]" : "h-0"} md:h-screen`}>
+        <div className={`z-[100] overflow-y-scroll overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 "h-[93vh]" md:left-[13vw] md:h-screen`}>
           {children}
           <div>
             <div className='z-[200] flex justify-center'>
